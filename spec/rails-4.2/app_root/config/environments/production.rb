ed00d8ded50b69f766b9b7753b1c33fff342fe23
Joe Khoobyar <joe@khoobyar.name>
2015-03-13 13:08:29 -0400

    target Rails 4.2

--- spec/rails-4.2/app_root/config/environments/production.rb
+++ spec/rails-4.2/app_root/config/environments/production.rb
@@ -2,7 +2,7 @@ CardiacTest::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_files = false
   config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
